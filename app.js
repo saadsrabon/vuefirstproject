507eedce17d679c3f4d18bb0d97d18a50995b881
Saad Srabon <saadsrabon2@gmail.com>
2024-10-21 23:09:21 +0600

    Assignment for vue

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 Vue.createApp({
-    data(){
-     return{
-        goals:[],
-        enteredValue:"",
-     };
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
     },
-    methods:{
-        addGoal(){
-            this.goals.push(this.enteredValue)
-        },
-    },
-
-}).mount("#app")
+  },
+}).mount("#app");
+// try to add new syntax for data
